7b9f5329f8d5250302b27a3937dc818f24655e17
sg-Oranj <85916343+sg-Oranj@users.noreply.github.com>
2022-11-28 16:00:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function shadowwizard(options) {
+    let images = document.quesrySelectorAll(".shadowwizard");
+
+    if (options.shadow_type === 'hard')
+         options.shadow_type = '0px'
+    else     
+}
